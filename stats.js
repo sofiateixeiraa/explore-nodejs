a68beba3c19093be8a2ecefbae23a7ceee764af0
Sofia Teixeira <st.sofiateixeira@gmail.com>
2022-03-05 00:57:46 +0000

    memory stats

---
+++ stats.js
@@ -0,0 +1,23 @@
+const os = require ('os') //import module
+
+//show free memory, memory total and free %
+
+const {freemem, totalmem} = os
+
+setInterval(()=> {
+    const total = parseInt(totalmem()/1024/1024)
+    const mem = parseInt(freemem()/1024/1024)
+    const percents = parseInt((mem/total)*100)
+    
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents} %`
+    }
+
+    console.clear()
+    console.log("=== PC STATS ===")
+    console.table(stats)
+
+
+}, 1000)
